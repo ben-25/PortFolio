1a9fc651a71974b22415d0037eb084599ede48bc
ben-25 <ben-2004@hotmail.fr>
2026-08-06 17:34:20 +0200

    Changement CV et optimisation des fonds 3d

--- src/components/HeroBackground.tsx
+++ src/components/HeroBackground.tsx
@@ -1,31 +1,31 @@
-import { Canvas, useFrame } from '@react-three/fiber'
-import { useRef } from 'react'
-import * as THREE from 'three'
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+import * as THREE from "three";
 
 function AnimatedSphere() {
-    const meshRef = useRef<THREE.Mesh>(null)
+  const meshRef = useRef<THREE.Mesh>(null);
 
-    useFrame(() => {
-        if (!meshRef.current) return
+  useFrame(() => {
+    if (!meshRef.current) return;
 
-        meshRef.current.rotation.y += 0.0003
-        meshRef.current.rotation.x += 0.0001
-    })
+    meshRef.current.rotation.y += 0.0003;
+    meshRef.current.rotation.x += 0.0001;
+  });
 
-    return (
-        <mesh ref={meshRef}>
-            <sphereGeometry args={[2.7, 200, 200]} />
-            <meshStandardMaterial color="#6c5ce7" wireframe />
-        </mesh>
-    )
+  return (
+    <mesh ref={meshRef}>
+      <sphereGeometry args={[2.7, 32, 32]} />
+      <meshStandardMaterial color="#6c5ce7" wireframe />
+    </mesh>
+  );
 }
 
 export function HeroBackground() {
-    return (
-        <Canvas camera={{ position: [0, 0, 5] }}>
-            <ambientLight intensity={1} />
-            <directionalLight position={[2, 2, 2]} />
-            <AnimatedSphere />
-        </Canvas>
-    )
+  return (
+    <Canvas camera={{ position: [0, 0, 5] }}>
+      <ambientLight intensity={1} />
+      <directionalLight position={[2, 2, 2]} />
+      <AnimatedSphere />
+    </Canvas>
+  );
 }
--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,94 +1,147 @@
-import '../style/Resume.css'
-import externe from '../assets/icons8-lien-externe.svg'
+import "../style/Resume.css";
+import externe from "../assets/icons8-lien-externe.svg";
+import CV from "../../dist/CV_Benjamin_Correa.pdf";
 
-export function Resume(){
-    return(
-        <>
-        <div className='resume'>
-            <section className="cv">
-                <section className='intro'>
-                    <h1>Benjamin Correa</h1>
-                    <h2>Développeur Full Stack - Recherche alternance CDA (Niveau 6 RNCP)</h2>
-                    <p>Lyon • <a href="mailto:ben-2004@hotmail.fr">ben-2004@hotmail.fr <img src={externe} alt="" /></a> • 06 48 32 48 96 • <a href="https://github.com/ben-25" target="_blank">github.com/ben-25 <img src={externe} alt="" /></a></p>
-                </section>
+export function Resume() {
+  return (
+    <>
+      <div className="resume">
+        <section className="cv">
+          <section className="intro">
+            <h1>Benjamin Correa</h1>
+            <h2>
+              Développeur Full Stack - Recherche alternance CDA (Niveau 6 RNCP)
+            </h2>
+            <p>
+              Disponible à partir du 21/09/2026 • 4 jour entreprise 1 jour école
+            </p>
+            <p>
+              Lyon •{" "}
+              <a href="mailto:ben-2004@hotmail.fr">
+                ben-2004@hotmail.fr <img src={externe} alt="" />
+              </a>{" "}
+              • 06 48 32 48 96 •{" "}
+              <a href="https://github.com/ben-25" target="_blank">
+                github.com/ben-25 <img src={externe} alt="" />
+              </a>
+            </p>
+          </section>
 
-                <section className="profil">
-                    <h3>Profil</h3>
-                    <p>
-                        Développeur web passionné, à la recherche d'une alternance pour préparer le titre de 
-                        Concepteur Développeur d'Applications (CDA). Maîtrise du développement Full Stack avec 
-                        React, JavaScript/Node.js, JAVA, SQL et Docker. Curieux, autonome et motivé à monter en 
-                        compétences dans un environnement professionnel.
-                    </p>
-                </section>
+          <section className="profil">
+            <h3>Profil</h3>
+            <p>
+              Développeur web passionné, à la recherche d'une alternance pour
+              préparer le titre de Concepteur Développeur d'Applications (CDA).
+              Maîtrise du développement Full Stack avec React,
+              JavaScript/Node.js, JAVA, SQL et Docker. Curieux, autonome et
+              motivé à monter en compétences dans un environnement
+              professionnel.
+            </p>
+          </section>
 
-                <section className="competences">
-                    <h3>Compétences Techniques</h3>
-                    <ul>
-                        <li><strong>Front-end :</strong> React, TypeScript, JavaScript (ES6+), HTML5, CSS3</li>
-                        <li><strong>Back-end :</strong> Node.js, JAVA</li>
-                        <li><strong>Bases de données :</strong> SQL, MySQL, PostgreSQL</li>
-                        <li><strong>Outils :</strong> Git / GitHub, REST API, Vite, Docker</li>
-                        <li><strong>IDE :</strong> VSCode</li>
-                        <li><strong>Méthodes :</strong> Responsive Design, MVC, bases des méthodes Agile</li>
-                    </ul>
-                </section>
+          <section className="competences">
+            <h3>Compétences Techniques</h3>
+            <ul>
+              <li>
+                <strong>Language :</strong> React, TypeScript, JavaScript
+                (ES6+), HTML5, CSS3, Node.js, JAVA, SQL
+              </li>
+              <li>
+                <strong>Bases de données :</strong>MySQL, PostgreSQL
+              </li>
+              <li>
+                <strong>Outils :</strong> Git / GitHub, REST API, Vite, Docker
+              </li>
+              <li>
+                <strong>IDE :</strong> VSCode, ZED
+              </li>
+              <li>
+                <strong>Méthodes :</strong> Responsive Design, MVC, bases des
+                méthodes Agile
+              </li>
+              <li>
+                <strong>Systeme d’exploitation : </strong> Windows
+              </li>
+            </ul>
+          </section>
 
-                <section className="projets">
-                    <h3>Projets Personnels</h3>
+          <section className="projets">
+            <h3>Projets Personnels</h3>
 
-                    <article>
-                        <h4>Music Search App — React + iTunes API</h4>
-                        <p>
-                            Application de recherche d'albums musicaux utilisant l'API iTunes. Gestion des appels 
-                            API asynchrones, affichage dynamique des résultats et interface responsive en React.
-                        </p>
-                    </article>
+            <article>
+              <h4>Music Search App - React + iTunes API</h4>
+              <p>
+                Application de recherche d'albums musicaux utilisant l'API
+                iTunes. Gestion des appels API asynchrones, affichage dynamique
+                des résultats et interface responsive en React.
+              </p>
+            </article>
 
-                    <article>
-                        <h4>Portfolio Personnel — React</h4>
-                        <p>
-                            Site vitrine développé en React/TypeScript présentant mes projets, compétences et parcours.
-                        </p>
-                    </article>
+            <article>
+              <h4>Portfolio Personnel - React</h4>
+              <p>
+                Site vitrine développé en React/TypeScript présentant mes
+                projets, compétences et parcours.
+              </p>
+            </article>
 
-                    <article>
-                        <h4>Projet FullStack — Node.js / React</h4>
-                        <p>
-                            Projet full‑stack complet avec dashboard : création d’un dashboard moderne, mise en place 
-                            d’une API sécurisée, gestion d’une base de données et intégration d’une interface React.
-                        </p>
-                    </article>
-                </section>
+            <article>
+              <h4>Projet de groupe FullStack - Node.js / React, TypeScript</h4>
+              <p>
+                Projet full‑stack complet réalisé en groupe : création d’un
+                gestionnaire d’organisation, mise en place d’une API sécurisée,
+                gestion d’une base de données et intégration d’une interface
+                React.
+              </p>
+            </article>
+          </section>
 
-                <section className="formation">
-                    <h3>Formation</h3>
-                    <ul>
-                        <li><strong>Acquis :</strong> Permis de conduire</li>
-                        <li><strong>Acquis :</strong> Bac Général — Option : Mathématiques, NSI, Sciences de l’ingénieur — Nancy, 2022</li>
-                        <li><strong>En cours :</strong> CDA (Concepteur Développeur d'Applications) — Titre RNCP Niveau 6 — Alternance recherchée — Lyon, 2026</li>
-                    </ul>
-                </section>
+          <section className="formation">
+            <h3>Formation</h3>
+            <ul>
+              <li>
+                <strong>En cours :</strong> CDA (Concepteur Développeur
+                d'Applications) — Titre RNCP Niveau 6 — Alternance recherchée —
+                Lyon, 2026
+              </li>
+              <li>
+                <strong>Ecole 42 : </strong>Passage de la piscine - 72/100
+                examens final
+              </li>
+              <li>
+                <strong>Université - Licence Communication : </strong> Cours
+                d’infographie et création de 0 d’un jeux de société
+              </li>
+              <li>
+                <strong>Acquis :</strong> Bac Général - Option : Mathématiques,
+                NSI, Sciences de l’ingénieur - Nancy, 2022
+              </li>
+            </ul>
+          </section>
 
-                <section className="savoir-etre">
-                    <h3>Savoir-être</h3>
-                    <ul>
-                        <li>Autonomie et capacité à apprendre seul (projets personnels)</li>
-                        <li>Curiosité technique et veille technologique continue</li>
-                        <li>Rigueur dans l'organisation du code et la documentation</li>
-                        <li>Esprit d'équipe et bonne communication</li>
-                    </ul>
-                </section>
+          <section className="savoir-etre">
+            <h3>Savoir-être</h3>
+            <ul>
+              <li>
+                Autonomie et capacité à apprendre seul (projets personnels)
+              </li>
+              <li>Curiosité technique et veille technologique continue</li>
+              <li>Rigueur dans l'organisation du code et la documentation</li>
+              <li>Esprit d'équipe et bonne communication</li>
+            </ul>
+          </section>
 
-                <section className="langues">
-                    <h3>Langues</h3>
-                    <p>Français : Langue maternelle — Anglais : Professionnel (lecture de documentation technique)</p>
-                </section>
-            </section>
-        </div>
-        <div className='resume-footer'>
-            <a href="/CV_Benjamin_Correa.pdf" download className='btn-download'>Telecharger mon CV</a>
-        </div>
-        </>
-    )
+          <section className="langues">
+            <h3>Langues</h3>
+            <p>Français : Langue maternelle - Anglais : B1</p>
+          </section>
+        </section>
+      </div>
+      <div className="resume-footer">
+        <a href={CV} download className="btn-download">
+          Telecharger mon CV
+        </a>
+      </div>
+    </>
+  );
 }
